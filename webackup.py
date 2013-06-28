891cff0993e6edd5028c3555b65aa7dda85bffb1
Oliver Day <oliverday@gmail.com>
2013-06-28 15:49:06 -0400

    recursive download now works. added download_if_newer function to prevent excessive downloads.

--- webackup.py
+++ webackup.py
@@ -1,22 +1,38 @@
 import ftputil
 import os.path
 
-dir_dest = '/tmp/ftptest/'
+# root directory for local host
+root_ldir = '/tmp/ftptest/'
+# root directory for ftp host
+root_fdir = 'MISC'
+
+#connect to the FTP server
 ftp = ftputil.FTPHost('ftp.microsoft.com','anonymous','')
-recursive = ftp.walk("MISC",topdown=True,onerror=None)
+
+# gather the remote file structure
+recursive = ftp.walk(root_fdir,topdown=True,onerror=None)
+
+
 for root,dirs,files in recursive:
     for fname in files:
         fpath = ftp.path.join(root, fname)
-        print "fpath %s" % fpath
         lpath = ftp.path.dirname(fpath)
-        print "lpath %s" % lpath
+        dest_dir = os.path.join(root_ldir, lpath)
 
+        # TODO if file is in exclude list skip
+
+        # if the local directory doesn't exist make it
         try:
-            os.stat(lpath)
+            os.stat(dest_dir)
         except:
-            os.makedirs(lpath)
-        print "joined path %s" % os.path.join(dir_dest, lpath)
+            os.makedirs(dest_dir)
 
-        #if ftp.path.isfile(fpath):
-            #ftp.download(fpath, os.path.join(dir_dest, lpath), 'b')
+        # download the file in the target directory
+        try:
+             if ftp.path.isfile(fpath):
+                target = os.path.join(dest_dir, fname)
+                ftp.download_if_newer(fpath,target, 'b')
+        except IOError, obj:
+            raise IOError("%s" % (obj.strerror))
+
 ftp.close
