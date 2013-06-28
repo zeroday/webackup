f3a8baa138f5933e52920fd468a440867b75c235
Oliver Day <oliverday@gmail.com>
2013-06-28 14:07:33 -0400

    now able to commit subdirectories but not download into it

--- webackup.py
+++ webackup.py
@@ -1,12 +1,22 @@
 import ftputil
 import os.path
+
 dir_dest = '/tmp/ftptest/'
 ftp = ftputil.FTPHost('ftp.microsoft.com','anonymous','')
 recursive = ftp.walk("MISC",topdown=True,onerror=None)
 for root,dirs,files in recursive:
     for fname in files:
         fpath = ftp.path.join(root, fname)
-        print fname, fpath
-        if ftp.path.isfile(fpath):
-            ftp.download(fpath, os.path.join(dir_dest, fname), 'b')
+        print "fpath %s" % fpath
+        lpath = ftp.path.dirname(fpath)
+        print "lpath %s" % lpath
+
+        try:
+            os.stat(lpath)
+        except:
+            os.makedirs(lpath)
+        print "joined path %s" % os.path.join(dir_dest, lpath)
+
+        #if ftp.path.isfile(fpath):
+            #ftp.download(fpath, os.path.join(dir_dest, lpath), 'b')
 ftp.close
