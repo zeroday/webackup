c870f0fd679c53c1e5f6f1461441c02118d6525e
Oliver Day <oliverday@gmail.com>
2013-06-29 15:49:45 -0400

    added function to exclude directories. also moved logic to download a file into a function.

--- webackup.py
+++ webackup.py
@@ -10,6 +10,9 @@ root_ldir = config['root_ldir']
 # root directory for ftp host
 root_fdir = config['root_fdir']
 
+# directories to exclude
+excluded_dirs = config['excluded_dirs']
+
 # ftp server credentials
 ftp_host = config['ftp_host']
 ftp_user = config['ftp_user']
@@ -21,6 +24,28 @@ ftp = ftputil.FTPHost(ftp_host, ftp_user, ftp_pass)
 # gather the remote file structure
 recursive = ftp.walk(root_fdir,topdown=True,onerror=None)
 
+def exclude_this_directory(current_dir):
+    for relative_dir in excluded_dirs:
+        excluded_dir = ftp.path.join(root_fdir, relative_dir)
+        target = current_dir
+        if (excluded_dir == target):
+            return True
+    return False
+
+def download_file(fpath, fname, lpath, dest_dir):
+    # if the directory doesn't exist locally then create it
+    try:
+        os.stat(dest_dir)
+    except:
+        os.makedirs(dest_dir)
+
+    # download the file into the target directory
+    try:
+        if ftp.path.isfile(fpath):
+            target = os.path.join(dest_dir, fname)
+            ftp.download_if_newer(fpath, target, 'b')
+    except IOError, obj:
+        raise IOError("%s" % (obj.strerror))
 
 for root,dirs,files in recursive:
     for fname in files:
@@ -28,20 +53,10 @@ for root,dirs,files in recursive:
         lpath = ftp.path.dirname(fpath)
         dest_dir = os.path.join(root_ldir, lpath)
 
-        # TODO if file is in exclude list skip
-
-        # if the local directory doesn't exist make it
-        try:
-            os.stat(dest_dir)
-        except:
-            os.makedirs(dest_dir)
-
-        # download the file in the target directory
-        try:
-             if ftp.path.isfile(fpath):
-                target = os.path.join(dest_dir, fname)
-                ftp.download_if_newer(fpath,target, 'b')
-        except IOError, obj:
-            raise IOError("%s" % (obj.strerror))
+        # exclude the directory if it is found in excluded_dirs
+        if exclude_this_directory(lpath):
+            continue
+        else:
+            download_file(fpath, fname, lpath, dest_dir)
 
 ftp.close
