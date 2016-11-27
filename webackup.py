ee9aab42d9b2cc2e905d42d9995f96d1970ddef1
Oliver Day <oliver.day@gmail.com>
2016-11-27 02:36:12 -0500

    more printing

--- webackup.py
+++ webackup.py
@@ -4,39 +4,39 @@ import yaml
 import re
 
 config = yaml.load(file("config.yml"))
-print config
+print(config)
 
 # root directory for local host
 root_ldir = config['root_ldir']
-print root_ldir
+print(root_ldir)
 
 # root directory for ftp host
 root_fdir = config['root_fdir']
-print root_fdir
+print(root_fdir)
 
 # directories to exclude
 excluded_dirs = config['excluded_dirs']
-print excluded_dirs
+print(excluded_dirs)
 
 excluded_files = config['excluded_files']
-print excluded_files
+print(excluded_files)
 
 excluded_filetypes = ['\.zip', '\.tar', '\.tar\.gz', '\.gz', '\.rar']
-print excluded_filetypes
+print(excluded_filetypes)
 
 # ftp server credentials
 ftp_host = config['ftp_host']
 ftp_user = config['ftp_user']
 ftp_pass = config['ftp_pass']
 
-print ftp_host, ftp_user, ftp_pass
+print(ftp_host, ftp_user, ftp_pass)
 
 # connect to the FTP server
 ftp = ftputil.FTPHost(ftp_host, ftp_user, ftp_pass)
-print ftp
+print(ftp)
 # gather the remote file structure
 recursive = ftp.walk(root_fdir, topdown=True, onerror=None)
-print recursive
+print(recursive)
 
 def exclude_this_directory(target):
     global excluded_dirs
@@ -104,12 +104,12 @@ def download_file(root_ldir, fpath, fname, lpath, dest_dir):
             target = os.path.join(dest_dir, fname)
             ftp.download_if_newer(fpath, target, 'b')
     except IOError, obj:
-        print obj.strerror
+        print(obj.strerror)
         # TODO exception handler for connection time out
         # reconnect and retry the download
         # all other exceptions are logged to file
     except ftputil.error.FTPOSError, exc:
-        print exc
+        print(exc)
 
 
 for root,dirs,files in recursive:
@@ -132,7 +132,7 @@ for root,dirs,files in recursive:
 
         # exclude the directory if it is found in excluded_dirs
         if exclude_this_directory(lpath):
-            print lpath
+            print(lpath)
             continue
         else:
             print(root_ldir, fpath, fname, lpath, dest_dir)
